e78550dd17cb8d0267b4920fec0d716012cc3289
Evan Yip <etyip@ucsb.edu>
2021-07-09 14:34:30 -0700

    Added Login Screen to Search Screen nav

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import { NativeRouter, Route, Link } from 'react-router-native';
 
 import LoginScreen from './app/screens/LoginScreen';
+import SearchScreen from './app/screens/SearchScreen';
 
+// const Stack = createStackNavigator();
+
+const Stack = createStackNavigator();
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            headerTitle: 'Search For A Person',
+            headerLeft: () => null,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 
-function LoginScreen(props) {
+function LoginScreen({ navigation }) {
+  const onLoginButtonPress = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Search' }],
+    });
+    navigation.push('Search');
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>What is going on</Text>
+      <Button title="Go to Search" onPress={onLoginButtonPress} />
     </View>
   );
 }
